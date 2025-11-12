cc737dc13501f71917e3e0e5b46eb2c99d4efaf0
Carole Trem <trem.carole@gmail.com>
2025-11-12 19:10:49 +0100

    creationEvent

---
+++ HappyGift/Features/EventCreation/ViewModel/EventViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  EventViewModel.swift
+//  HappyGift
+//
+//  Created by caroletm on 12/11/2025.
+//
+
+import Foundation
+import Observation
+import SwiftUI
+
+@Observable
+
+class EventViewModel {
+    
+    //MARK: -  Map Data
+    
+    var iconEvent : [String] = ["carChristmas", "iconChristmas", "houseChristmas"]
+    
+    
+}
+
+
--- HappyGift/Features/EventCreation/Views/CreateSanta.swift
+++ HappyGift/Features/EventCreation/Views/CreateSanta.swift
@@ -8,8 +8,107 @@
 import SwiftUI
 
 struct CreateSanta: View {
+    
+    @State var nomEvent: String = ""
+    @State var descriptionEvent: String = ""
+    @State var iconEvent: String = ""
+    @State var dateEvent: Date = Date()
+    @State var lieuEvent: String = ""
+    
     var body: some View {
-        Text("Create Santa")
+        ZStack {
+            Color.beige.edgesIgnoringSafeArea(.all)
+            VStack {
+                VStack {
+                    Image(.littleSantaClaus)
+//                    Text("Nouveau Secret Santa")
+//                        .font(.custom("Syncopate-Bold", size: 13))
+//                        .foregroundStyle(.rouge)
+                        
+                }
+                ScrollView {
+                    VStack {
+                        
+                        Text("Nom de l'evenement")
+                            .font(.custom("Syncopate-Bold", size: 15))
+                            .foregroundStyle(.rougeDark)
+                            .padding(.top, 10)
+                        
+                        TextField("Nom de l'évènement", text: $nomEvent)
+                            .frame(width: 340, height: 40, alignment: .center)
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .padding()
+                        
+                        Text("Icone de l'evenement")
+                            .font(.custom("Syncopate-Bold", size: 15))
+                            .foregroundStyle(.rougeDark)
+                        
+                        HStack {
+                            ForEach(0..<3, id: \.self) { icon in
+                                IconCard(image:"carChristmas")
+                            }
+                        }.padding()
+                        
+                        
+                        Text("Description de l'evenement")
+                            .font(.custom("Syncopate-Bold", size: 15))
+                            .foregroundStyle(.rougeDark)
+                        TextEditor(text: $descriptionEvent)
+                            .frame(width: 340, height: 135, alignment: .center)
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .padding()
+                        
+                
+                        
+                        Text("Lieu de l'evenement")
+                            .font(.custom("Syncopate-Bold", size: 15))
+                            .foregroundStyle(.rougeDark)
+                        TextField("Lieu", text: $lieuEvent)
+                            .frame(width: 340, height: 40, alignment: .center)
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .padding()
+                        
+                        Text("Date de l'evenement")
+                            .font(.custom("Syncopate-Bold", size: 15))
+                            .foregroundStyle(.rougeDark)
+                            .padding()
+                        
+                        HStack {
+                           
+                            Button {
+                                //
+                            }label: {
+                                Text("Jour")
+                            }
+                            Button {
+                                //
+                            }label: {
+                                Text("Mois")
+                            }
+                            Button {
+                                //
+                            }label: {
+                                Text("Heure")
+                            }
+                            Button {
+                                //
+                            }label: {
+                                Text("Minute")
+                            }
+
+        
+                        }
+                        
+                        ButtonText(text: "OK", width: 150)
+                            .padding()
+                        
+                    }
+                }
+            }
+        }
     }
 }
 
