fd6f4c993e57d17878dc582c0099abb34a661184
“Samara <samara.limads@gmail.com>
2025-11-12 15:03:15 +0100

    first commit

---
+++ HappyGift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HappyGift
+//
+//  Created by Samara Lima da Silva on 12/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HappyGift/HappyGiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyGiftApp.swift
+//  HappyGift
+//
+//  Created by Samara Lima da Silva on 12/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct HappyGiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
